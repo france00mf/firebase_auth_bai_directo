fad4ab49d4f0cbfe6ceed97644c4c8550d41648d
France <franciscodomingosjose06798@gmail.com>
2023-06-15 07:10:21 +0100

    create app file

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  BuildContext(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}

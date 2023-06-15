f3de4f8164270c00ec573b6239d5e210159379b2
France <franciscodomingosjose06798@gmail.com>
2023-06-15 07:15:17 +0100

    import a new class

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_auth_bai_directo/presenter/views/home/home_view.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   @override
   BuildContext(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      home: HomeView(),
     );
   }
 }

33eb8163b9417d8bdda4bbcc6a3b3df634694d3c
France <franciscodomingosjose06798@gmail.com>
2023-06-15 08:08:38 +0100

    fix: implement myRoutes var

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../presenter/presenter.dart';
+import 'app.dart';
 
 class App extends StatelessWidget {
   @override
   build(BuildContext context) {
     return MaterialApp(
       home: HomeView(),
+      routes: myRoutes,
     );
   }
 }

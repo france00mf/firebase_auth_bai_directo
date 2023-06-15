f305fee8d8368102993601fc6c5df6f3aba0908b
France <franciscodomingosjose06798@gmail.com>
2023-06-15 08:12:41 +0100

    assing login view on routes

--- lib/app/utils/routes.dart
+++ lib/app/utils/routes.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth_bai_directo/presenter/views/auth/login/login_view.dart';
+
 import '../../presenter/presenter.dart';
 
 var myRoutes = {
   HomeView.idScreem: (_) => HomeView(),
+  LoginView.idScreem: (_) => LoginView.idScreem,
 };

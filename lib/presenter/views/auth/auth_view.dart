9c3bcfeca188ae8fe2208c5f5bdad554587ae614
France <franciscodomingosjose06798@gmail.com>
2023-06-19 11:39:26 +0100

    finesh auth_view page

--- lib/presenter/views/auth/auth_view.dart
+++ lib/presenter/views/auth/auth_view.dart
@@ -1,8 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_bai_directo/presenter/views/auth/login/login_view.dart';
 import 'package:flutter/material.dart';
+
+import '../../presenter.dart';
 
 class AuthView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return HomeView();
+            } else {
+              return LoginView();
+            }
+          }),
+    );
   }
 }

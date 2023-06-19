f5c3adc83642c8533c59658eef9ed24a798af6ec
France <franciscodomingosjose06798@gmail.com>
2023-06-19 11:41:54 +0100

    LoginOrRegister

--- lib/presenter/views/auth/auth.dart
+++ lib/presenter/views/auth/auth.dart
@@ -2,3 +2,4 @@ export 'forgot_password/forgot_password.dart';
 export 'login/login.dart';
 export 'otp/otp.dart';
 export 'register/register.dart';
+export 'login_or_register/login_or_register.dart';
---
+++ lib/presenter/views/auth/login_or_register/login_or_register.dart
@@ -0,0 +1,1 @@
+export 'login_or_register_view.dart';

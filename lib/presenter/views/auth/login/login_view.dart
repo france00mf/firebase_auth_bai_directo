03eee7efc5198d2d071eea485b1ba8c063728953
France <franciscodomingosjose06798@gmail.com>
2023-06-19 11:51:21 +0100

    add funtion onTap on LoginView to change ... on LoginOrRegister

--- lib/presenter/views/auth/login/login_view.dart
+++ lib/presenter/views/auth/login/login_view.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   static const idScreem = "login_view";
+  final Function()? onTap;
+  LoginView({super.key, required this.onTap})
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView();

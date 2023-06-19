32ea7b929c3ff6ed070abcb75a4b9867016ed6a5
France <franciscodomingosjose06798@gmail.com>
2023-06-19 11:34:24 +0100

    add first structure on authview

--- lib/presenter/views/auth/auth_view.dart
+++ lib/presenter/views/auth/auth_view.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
-class AuthView extends StatelessWidget {}
+class AuthView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

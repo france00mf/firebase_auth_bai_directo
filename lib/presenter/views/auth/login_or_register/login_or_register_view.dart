59220a7f2476833a33b6eb91a845c980a0a6862c
France <franciscodomingosjose06798@gmail.com>
2023-06-19 11:46:09 +0100

    add loginOrRegister first assets

--- lib/presenter/views/auth/login_or_register/login_or_register_view.dart
+++ lib/presenter/views/auth/login_or_register/login_or_register_view.dart
@@ -1,1 +1,17 @@
-class LoginOrRegister {}
+import 'package:flutter/material.dart';
+
+import '../../../presenter.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  @override
+  _LoginOrRegister createState() {
+    return _LoginOrRegister();
+  }
+}
+
+class _LoginOrRegister extends State<LoginOrRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return LoginView();
+  }
+}
